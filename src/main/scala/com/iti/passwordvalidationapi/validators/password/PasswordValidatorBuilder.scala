ff76e7fb4da09ae423a6683ff092b39a0f008fec
khwesten <k-heiner@hotmail.com>
2020-04-27 17:42:24 -0300

    Refactor on PasswordValidatorBuilder

--- src/main/scala/com/iti/passwordvalidationapi/validators/password/PasswordValidatorBuilder.scala
+++ src/main/scala/com/iti/passwordvalidationapi/validators/password/PasswordValidatorBuilder.scala
@@ -5,11 +5,6 @@ import com.iti.passwordvalidationapi.validators.password.PasswordValidator
 class PasswordValidatorBuilder(validators: PasswordValidator*) {
 
   def isValid(password: String): Boolean = {
-    val isInvalid = validators
-      .map(validator => validator.validate(password))
-      .filter(!_)
-      .size > 0
-
-    !isInvalid
+    validators.filter(validator => !validator.validate(password)).isEmpty
   }
 }
