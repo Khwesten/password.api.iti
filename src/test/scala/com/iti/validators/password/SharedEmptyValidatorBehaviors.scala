b66fcfb666cd27540a75bee88f4a59fec48c58fc
khwesten <k-heiner@hotmail.com>
2020-04-27 18:55:03 -0300

    Create SharedEmptyValidatorBehaviors

---
+++ src/test/scala/com/iti/validators/password/SharedEmptyValidatorBehaviors.scala
@@ -0,0 +1,12 @@
+package com.iti.validators.password
+
+import org.scalatest.WordSpec
+import com.iti.passwordvalidationapi.validators.password.PasswordValidator
+
+trait SharedEmptyValidatorBehaviors { this: org.scalatest.WordSpec =>
+  def isEmpty(validator: PasswordValidator) {
+    "password is empty" in {
+      assert(!validator.validate(""))
+    }
+  }
+}
