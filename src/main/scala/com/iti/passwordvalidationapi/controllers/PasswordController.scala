e915362eef2d1236034060e4760f0b80b3e3bbdd
khwesten <k-heiner@hotmail.com>
2020-04-28 19:44:30 -0300

    Implement validatePassword endpoint

--- src/main/scala/com/iti/passwordvalidationapi/controllers/PasswordController.scala
+++ src/main/scala/com/iti/passwordvalidationapi/controllers/PasswordController.scala
@@ -1,15 +1,27 @@
 package com.iti.passwordvalidationapi.controllers
 
+import com.iti.passwordvalidationapi.contracts.PasswordContract
+import com.iti.passwordvalidationapi.validators.password.DefaultPasswordValidatorBuilder
+
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 
+import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
+import io.circe.generic.auto._
+
 class PasswordController extends BaseController {
 
-  private def validation = post {
-    complete("OK!")
+  private def validatePassword = path("validatePassword") {
+    post {
+      entity(as[PasswordContract]) { contract =>
+        validate(contract.validate, contract.errorMessage) {
+          val validatorBuilder = DefaultPasswordValidatorBuilder.build()
+
+          complete(validatorBuilder.isValid(contract.password))
+        }
+      }
+    }
   }
 
-  val passwordRoutes = pathPrefix("passwords") {
-    validation
-  }
+  val passwordRoutes = validatePassword
 }
