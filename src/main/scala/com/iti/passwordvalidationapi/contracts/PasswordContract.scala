4a31d4d21e42106d608e213a9df6562a73071b3f
khwesten <k-heiner@hotmail.com>
2020-04-28 22:17:52 -0300

    Remove validate from PasswordContract

--- src/main/scala/com/iti/passwordvalidationapi/contracts/PasswordContract.scala
+++ src/main/scala/com/iti/passwordvalidationapi/contracts/PasswordContract.scala
@@ -1,6 +1,3 @@
 package com.iti.passwordvalidationapi.contracts
 
-case class PasswordContract(password: String) {
-  def validate(): Boolean = password.nonEmpty
-  def errorMessage(): String = s"Password cannot be empty."
-}
+case class PasswordContract(password: String)
